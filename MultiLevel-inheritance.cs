2e8e8fa8aff3a0e8a33dfd293cd838eb8725691d
Priyansh0211 <146169394+Priyansh0211@users.noreply.github.com>
2024-06-15 19:27:59 +0530

    15/06/2024

---
+++ MultiLevel-inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.Common;
+
+namespace mulinheritance
+{
+    class Cat
+    {
+        public void printcat()
+        {
+            Console.WriteLine("Cat has four leg");
+        }
+    }
+
+    class Loin : Cat
+    {
+        public void printLoin()
+        {
+            Console.WriteLine("Loin has four leg");
+        }
+    }
+
+    class dog : Loin
+    {
+        public void printdog()
+        {
+            Console.WriteLine("Dog has four leg");
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            dog dog = new dog();    
+
+            dog.printdog();
+            dog.printLoin();
+            dog.printcat();
+        }
+    }
+
+}
