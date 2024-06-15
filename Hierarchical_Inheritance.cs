d340f879d89aa61a1e7a3991343d14f45016b41f
Priyansh0211 <146169394+Priyansh0211@users.noreply.github.com>
2024-06-15 17:15:21 +0530

    15/06/2024

---
+++ Hierarchical_Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LearningInheritance1
+{
+    class ParentClass1
+    {
+        public string name;
+        public int Id = 10;
+
+        public void displayParentClassDetail()
+        {
+            Console.WriteLine($"I am {name}");
+            Console.WriteLine($"ID : {Id}");
+        }
+    }
+
+    class ChildClass1 : ParentClass1
+    {
+        public void getIdFromParentClass()
+        {
+            Console.WriteLine("From my child class");
+            Console.WriteLine($"ID : {Id}");
+        }
+    }
+
+    class anotherChildClass1 : ParentClass1 
+    {
+        public void getIdFromParentClass()
+        {
+            Console.WriteLine("From my anotherchild class");
+            Console.WriteLine($"ID : {Id}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            anotherChildClass1 child = new anotherChildClass1();
+
+            child.getIdFromParentClass();
+        }
+    }
+}
---
+++ simple-inheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LearningInheritance
+{
+    class ParentClass
+    {
+        public string name;
+        public int Id = 9;
+
+        public void displayParentClassDetails()
+        {
+            Console.WriteLine($"I am {name}");
+            Console.WriteLine($"ID : {Id}");
+        }
+    }
+
+    class ChildClass : ParentClass
+    {
+        public void getIdFromParentClass()
+        {
+            Console.WriteLine($"This is mt ID : {Id}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass child = new ChildClass();
+            child.name = "Priyansh";
+            child.getIdFromParentClass();
+        }
+    }
+}
